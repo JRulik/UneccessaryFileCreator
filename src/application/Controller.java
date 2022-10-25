a18882857a6fd705ee2bbb3a3bad791ccdc4ef9a
JRulik <jan.rulik@atlas.cz>
2022-10-25 11:34:08 +0200

    Pridana komplet funkce checkboxum

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -2,8 +2,10 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class Controller {
 
@@ -11,70 +13,120 @@ public class Controller {
 	TextField textTimeOut;
 	@FXML
 	TextField textCountOfFiles;
-		
+	@FXML
+	CheckBox checkBoxUnlimited;
+	@FXML
+	CheckBox checkBoxTimeOut;
+	
 	public void  scrollSize(ActionEvent event) {
 		System.out.println("Butt1");
 	}
 	
 	public void countOfFilesPlus(ActionEvent event) {
 	
-		try {
-			int count = Integer.parseInt(textCountOfFiles.getText());
-			count++;
-			textCountOfFiles.setText(Integer.toString(count));
+		if (!this.checkBoxUnlimited.isSelected()) {
+			try {
+				int count = Integer.parseInt(textCountOfFiles.getText());
+				count++;
+				textCountOfFiles.setText(Integer.toString(count));
+			}
+			catch (NumberFormatException e) {
+				textCountOfFiles.setText("ERR");
+			}
+			catch (Exception e) {
+				System.out.println(e);
+			}		
 		}
-		catch (NumberFormatException e) {
-			textCountOfFiles.setText("ERR");
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+
 		
 	}
 	
 	public void countOfFilesMinus(ActionEvent event) {
-		try {
-			int count = Integer.parseInt(textCountOfFiles.getText());
-			if (count>1) {
-				count--;
-				textCountOfFiles.setText(Integer.toString(count));
+		
+		if (!this.checkBoxUnlimited.isSelected()) {
+			try {
+				int count = Integer.parseInt(textCountOfFiles.getText());
+				if (count>1) {
+					count--;
+					textCountOfFiles.setText(Integer.toString(count));
+				}
 			}
-		}
-		catch (NumberFormatException e) {
-			textCountOfFiles.setText("ERR");
-		}
-		catch (Exception e) {
-			System.out.println(e);
+			catch (NumberFormatException e) {
+				textCountOfFiles.setText("ERR");
+			}
+			catch (Exception e) {
+				System.out.println(e);
+			}
 		}
 	}
 	
 	public void timeOutPlus(ActionEvent event) {
-		try {
-			int count = Integer.parseInt(textTimeOut.getText());
-			count++;
-			textTimeOut.setText(Integer.toString(count));
+	
+		if (this.checkBoxTimeOut.isSelected()) {
+			try {
+				int count = Integer.parseInt(textTimeOut.getText());
+				count++;
+				textTimeOut.setText(Integer.toString(count));
+			}
+			catch (NumberFormatException e) {
+				textTimeOut.setText("ERR");
+			}
+			catch (Exception e) {
+				System.out.println(e);
+			}
 		}
-		catch (NumberFormatException e) {
-			textTimeOut.setText("ERR");
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+		
 	}
 	
 	public void timeOutMinus(ActionEvent event) {
-		try {
-			int count = Integer.parseInt(textTimeOut.getText());
-			if (count>0) {
-				count--;
-				textTimeOut.setText(Integer.toString(count));
+		
+		if (this.checkBoxTimeOut.isSelected()) {
+			try {
+				int count = Integer.parseInt(textTimeOut.getText());
+				if (count>0) {
+					count--;
+					textTimeOut.setText(Integer.toString(count));
+				}
+			}
+			catch (NumberFormatException e) {
+				textTimeOut.setText("ERR");
+			}
+			catch (Exception e) {
+				System.out.println(e);
 			}
 		}
-		catch (NumberFormatException e) {
-			textTimeOut.setText("ERR");
+	}
+	
+	public void unlimitedOnOff(ActionEvent event) {	
+		if ( this.checkBoxUnlimited.isSelected()) {
+			textCountOfFiles.setEditable(false);
+			textCountOfFiles.setText("\u221e");
+			textCountOfFiles.getStyleClass().remove("enabled");
+			textCountOfFiles.getStyleClass().add("disabled");
+			
 		}
-		catch (Exception e) {
-			System.out.println(e);
+		else {
+			textCountOfFiles.setEditable(true);
+			textCountOfFiles.setText("1");
+			textCountOfFiles.getStyleClass().remove("disabled");
+			textCountOfFiles.getStyleClass().add("enabled");
+		}
+	}
+	
+	public void timeOutOnOff(ActionEvent event) {
+		textTimeOut.getStyleClass().remove("textTimeOut");
+		if (!this.checkBoxTimeOut.isSelected()) {
+			textTimeOut.setText("0");
+			textTimeOut.setEditable(false);
+			textTimeOut.getStyleClass().remove("enabled");
+			textTimeOut.getStyleClass().add("disabled");
+			
+		}
+		else {
+			textTimeOut.setEditable(true);
+			//textTimeOut.setText("0");
+			textTimeOut.getStyleClass().remove("disabled");
+			textTimeOut.getStyleClass().add("enabled");
 		}
 	}
 	
