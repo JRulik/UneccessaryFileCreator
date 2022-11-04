8d457e7d331cfaa7826e84e7ca5232882f5597e9
JRulik <jan.rulik@atlas.cz>
2022-11-04 15:41:56 +0100

    Used maven for building With jlink compiled project. Some errors with jpackage (compiled, but don´ẗ run).

--- src/module-info.java
+++ src/module-info.java
@@ -5,5 +5,5 @@ module UneccessaryFileCreator {
 	requires javafx.fxml;
 	requires javafx.base;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens application to javafx.controls,javafx.graphics, javafx.fxml;
 }
